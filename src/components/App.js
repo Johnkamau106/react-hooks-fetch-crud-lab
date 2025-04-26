38a5cd24f8a2485f33422180ecd2f467275b95a0
johnkamau106 <john.kamau7@student.moringaschool.com>
2025-04-26 15:21:27 +0300

    added drop down to the question

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,65 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import AdminNavBar from "./AdminNavBar";
 import QuestionForm from "./QuestionForm";
 import QuestionList from "./QuestionList";
 
 function App() {
+  const [questions, setQuestions] = useState([]);
   const [page, setPage] = useState("List");
+
+  // Fetch questions from API
+  useEffect(() => {
+    fetch("http://localhost:4000/questions")
+      .then((response) => response.json())
+      .then((data) => setQuestions(data));
+  }, []);
+
+  // Add a new question
+  function handleAddQuestion(newQuestion) {
+    setQuestions([...questions, newQuestion]);
+  }
+
+  // Delete a question
+  function handleDeleteQuestion(id) {
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "DELETE",
+    })
+      .then(() => {
+        setQuestions(questions.filter((question) => question.id !== id));
+      });
+  }
+
+  // Update correct answer index
+  function handleUpdateCorrectAnswer(id, correctIndex) {
+    // Find and update the question with the new correctIndex
+    setQuestions((prevQuestions) =>
+      prevQuestions.map((question) =>
+        question.id === id
+          ? { ...question, correctIndex: correctIndex }
+          : question
+      )
+    );
+
+    // Send the update to the server
+    fetch(`http://localhost:4000/questions/${id}`, {
+      method: "PATCH",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ correctIndex }),
+    });
+  }
 
   return (
     <main>
       <AdminNavBar onChangePage={setPage} />
-      {page === "Form" ? <QuestionForm /> : <QuestionList />}
+      {page === "Form" ? (
+        <QuestionForm onAddQuestion={handleAddQuestion} />
+      ) : (
+        <QuestionList
+          questions={questions}
+          onDelete={handleDeleteQuestion}
+          onUpdate={handleUpdateCorrectAnswer}
+        />
+      )}
     </main>
   );
 }
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,20 @@
 import React from "react";
+import QuestionItem from "./QuestionItem";
 
-function QuestionList() {
+function QuestionList({ questions, onDelete, onUpdate }) {
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/*  */}</ul>
+      <ul>
+        {questions.map((question) => (
+          <QuestionItem
+            key={question.id}
+            question={question}
+            onDelete={onDelete}
+            onUpdate={onUpdate}
+          />
+        ))}
+      </ul>
     </section>
   );
 }
